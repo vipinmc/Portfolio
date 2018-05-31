9ef365364b871a347cbcf64c4e05d15cbc3fbf4b
VIPIN\vipin <vipinmc08@gmail.com>
2018-05-31 08:28:22 +0530

    1. Created Components 2. Converted CSS to SCSS 3. Nesting done completely

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from './header/index';
+import { FooterComponent } from './footer/index';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'My First Angular Application';
