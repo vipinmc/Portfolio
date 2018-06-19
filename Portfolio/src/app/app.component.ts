32c61aab61d5a2dac72d1701c63e59706e6b7e1b
VIPIN\vipin <vipinmc08@gmail.com>
2018-06-19 16:54:00 +0530

    Cleaned and started from first

--- Portfolio/src/app/app.component.ts
+++ Portfolio/src/app/app.component.ts
@@ -1,7 +1,4 @@
-import { Component, OnInit, ViewChild, Input } from '@angular/core';
-import { HeaderComponent } from './common/header/index';
-import { FooterComponent } from './common/footer/index';
-
+import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,28 +6,11 @@ import { FooterComponent } from './common/footer/index';
 })
 export class AppComponent implements OnInit {
 
-  public hello = "Hello";
-  public goodBye: string;
-
-  @ViewChild('myInputText') inputText;
-
   constructor(){}
 
   ngOnInit(){
-       setInterval(() => {this.send2Server();},2000)
+       
    }
-
-  title = 'My First Angular Application';
-
-  //Output
-  public setDataFromChild(data) {
-    this.goodBye = data;
-  }
-  //Viewchild
-  public send2Server() {
-    let data = this.inputText.nativeElement;
-    console.log(data.value)
-  }
 
 }
 
--- Portfolio/src/app/app.module.ts
+++ Portfolio/src/app/app.module.ts
@@ -1,26 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './common/header/index';
-import { FooterComponent } from './common/footer/footer.component';
-import { HomeComponent } from './home/home.component';
-import { ModalPopupComponent } from './components/modal-popup/modal-popup.component';
-import { ChildComponent } from './child/child.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent,
-    FooterComponent,
-    HomeComponent,
-    ModalPopupComponent,
-    ChildComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
